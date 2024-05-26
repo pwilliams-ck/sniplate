a4c946c7240bc65a4ce9f7d80edfbd9bc35d98c6
pwilliams-ck <pwilliams@cloudkey.io>
2024-05-26 01:42:39 -0500

    Update create snip handler

--- cmd/api/snips.go
+++ cmd/api/snips.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -10,7 +11,24 @@ import (
 
 // For now we simply return a plain-text placeholder response.
 func (app *application) createSnipHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Create a new snip.")
+	// Declare an anonymous struct to hold the information that we expect to be in the
+	// HTTP request body. This struct will be our *target decode destination*.
+	var input struct {
+		Title   string   `json:"title"`   // Snip title
+		Content string   `json:"content"` // Content of the snip
+		Tags    []string `json:"tags"`    // Slice of tags for the snip
+	}
+
+	// Initialize a new json.Decoder instance which reads from the request body, and
+	// then use the Decode() method to decode the body contents into the input struct.
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	// Dump contents of input struct in a HTTP response.
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 // "GET /v1/snips/:id" endpoint. For now, we retrieve the "id" parameter from the
